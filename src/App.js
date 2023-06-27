e5a0e94cef226402fcf29217504c9ede85654235
mdamaan853 <mdamaan853@gmail.com>
2023-06-27 21:22:00 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"
 import CardDetails from "./components/CartDetails"
+import Home from './components/Home';
+import ChatPage from './components/ChatPage/Chatpage';
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:4000');
 
 function App() {
   return (
     <>
-    <Header/>
-    <CardDetails/>
+    {/* <Header/> */}
+        <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+    {/* <CardDetails/> */}
     </>
   );
 }
